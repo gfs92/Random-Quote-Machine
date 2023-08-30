81f6cc282cf48e70e4af1096253c28e90a7f31c5
Grant Skinner <gfs00200@gmail.com>
2023-08-30 15:47:04 -0500

    Quotations and Tweet Button

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ function setQuote() {
       accept: "application/json",
     },
     success: function (data) {
-      $("#text").text(data.quote);
-      $("#author").text(data.character);
+      $("#text").text('"' + data.quote + '"');
+      $("#author").text("-" + data.character);
       // data is a quote in JSON form
       //   {
       //     "id": 307,
